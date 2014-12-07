2f8ab1f077e29030a97246434065e0c1e7e780a4
gambin_n <gambin_n@epitech.eu>
2014-12-07 12:20:49 +0900

    script bouton USE

---
+++ Assets/Scripts/ButtonAction.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var Range : float = 1;
+var MinAngle : float = 90;
+
+function Start () {
+
+}
+
+function pushedButtonCallback () {
+	Debug.Log("button pressed");
+}
+
+function Update () {
+	if (Input.GetButtonUp("Use"))
+	{
+		var player = GameObject.Find("First Person Controller").transform;
+		if (Vector2.Distance(player.position, transform.position) <= Range)
+		{
+	     	var cubeDir = transform.position - player.position;
+		    var angle = Vector3.Angle(cubeDir, player.forward);
+		    if (angle < MinAngle) {
+		    	pushedButtonCallback();
+		    }
+		}
+	}
+}
