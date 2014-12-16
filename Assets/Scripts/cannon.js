8e1f7f645c5887b874a963f50656886e81984d6e
gambin_n <gambin_n@epitech.eu>
2014-12-17 03:48:14 +0900

    clic pour activer item

--- Assets/Scripts/cannon.js
+++ Assets/Scripts/cannon.js
@@ -3,35 +3,52 @@ var speed = 30.0;
 var angle = 5.0;
 var cannon : GameObject;
 var level : GameObject;
+var Range : float = 1;
+var MinAngle : float = 90;
 var generationPoint : Transform;
 var ammo : int = 5;
 private var generated : int = 0;
 private var count : int = 0;
 
-function OnTriggerEnter (collision : Collider) {
-if (cannon.rigidbody.constraints == RigidbodyConstraints.FreezeRotation){
-if (generated < ammo && collision.gameObject.tag == "Player")
+
+function pushedButtonCallback ()
 {
-	var name : String = cannon.transform.name + "_ballClone" + generated.ToString();
-	generated++;
-	var ballClone : Rigidbody = Instantiate(ball, generationPoint.position, generationPoint.rotation);
-	ballClone.name = name;
-	ballClone.transform.parent = level.transform;
-	ballClone.velocity = cannon.transform.right * speed;
-	ballClone.velocity.y = angle;		
-	}
-else {
-	if (collision.gameObject.tag == "Player")
+	if (cannon.rigidbody.constraints == RigidbodyConstraints.FreezeRotation){
+	if (generated < ammo)
 	{
-		var names : String = cannon.transform.name + "_ballClone" + count.ToString();
-		count = (count + 1) % ammo;
-		var ballBis : GameObject = GameObject.Find(names);
-		ballBis.renderer.enabled = false;
-		ballBis.rigidbody.transform.position = generationPoint.position;
-		ballBis.renderer.enabled = true;
-		ballBis.rigidbody.velocity = cannon.transform.right * speed;
-		ballBis.rigidbody.velocity.y = angle;	
+		var name : String = cannon.transform.name + "_ballClone" + generated.ToString();
+		generated++;
+		var ballClone : Rigidbody = Instantiate(ball, generationPoint.position, generationPoint.rotation);
+		ballClone.name = name;
+		ballClone.transform.parent = level.transform;
+		ballClone.velocity = cannon.transform.right * speed;
+		ballClone.velocity.y = angle;		
+		}
+	else {
+
+			var names : String = cannon.transform.name + "_ballClone" + count.ToString();
+			count = (count + 1) % ammo;
+			var ballBis : GameObject = GameObject.Find(names);
+			ballBis.renderer.enabled = false;
+			ballBis.rigidbody.transform.position = generationPoint.position;
+			ballBis.renderer.enabled = true;
+			ballBis.rigidbody.velocity = cannon.transform.right * speed;
+			ballBis.rigidbody.velocity.y = angle;
 		}
 	}
+}
+
+function Update () {
+	if (Input.GetButtonUp("Fire1"))
+	{
+		var player = GameObject.Find("First Person Controller").transform;
+		if (Vector2.Distance(player.position, transform.position) <= Range)
+		{
+	     	var cubeDir = transform.position - player.position;
+		    var angle = Vector3.Angle(cubeDir, player.forward);
+		    if (angle < MinAngle) {
+		    	pushedButtonCallback();
+		    }
+		}
 	}
 }
--- Assets/Scripts/createCube.js
+++ Assets/Scripts/createCube.js
@@ -1,4 +1,9 @@
 ﻿//var Animator : GameObject;
+
+#pragma strict
+
+var Range : float = 1;
+var MinAngle : float = 90;
 var cube : Rigidbody;
 var speed = 10.0;
 var button : GameObject;
@@ -7,18 +12,15 @@ var ammo : int = 5;
 
 private var generated : int = 0;
 
-function OnTriggerEnter (collision : Collider) {
-
-if (collision.gameObject.tag == "Player" && generated >= ammo)
+function pushedButtonCallback () {
+	if (generated >= ammo)
 	{
-	var num : int = generated - ammo;
-	var names : String = button.transform.name + "_cubeClone" + num.ToString();
-	var cubeBis : GameObject = GameObject.Find(names);
-	cubeBis.SendMessage("explosion");
-	Destroy(cubeBis);
+		var num : int = generated - ammo;
+		var names : String = button.transform.name + "_cubeClone" + num.ToString();
+		var cubeBis : GameObject = GameObject.Find(names);
+		cubeBis.SendMessage("explosion");
+		Destroy(cubeBis);
 	}
-
-if (collision.gameObject.tag == "Player"){
 	var name : String = button.transform.name + "_cubeClone" + generated.ToString();
 	generated++;
 	var cubeClone : Rigidbody = Instantiate(cube, spawnLocation.position, spawnLocation.rotation);
@@ -26,5 +28,19 @@ if (collision.gameObject.tag == "Player"){
 	cubeClone.transform.parent = this.transform;
     cubeClone.velocity = button.transform.right * (-speed);
     audio.Play();
+}
+
+function Update () {
+	if (Input.GetButtonUp("Fire1"))
+	{
+		var player = GameObject.Find("First Person Controller").transform;
+		if (Vector2.Distance(player.position, transform.position) <= Range)
+		{
+	     	var cubeDir = transform.position - player.position;
+		    var angle = Vector3.Angle(cubeDir, player.forward);
+		    if (angle < MinAngle) {
+		    	pushedButtonCallback();
+		    }
+		}
 	}
 }
