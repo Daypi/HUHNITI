0ed7d9aaf0608f85619ccebca5675cff89ff6fa4
gambin_n <gambin_n@epitech.eu>
2014-12-16 16:02:44 +0900

    transition entre lvl

---
+++ Assets/Scripts/EndLevel.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var nextLevel = "Menu";
+
+function Start () {
+	//when a scene is loaded, fade from black
+	GameObject.Find("FadeInOut").animation.Play("FadeOutEnterLevel");
+}
+
+function Update () {
+
+}
+
+function OnTriggerStay()
+{
+	GameObject.Find("FadeInOut").animation.Play("FadeOutLeaveLevel");
+	yield WaitForSeconds(1);
+	Application.LoadLevel(nextLevel);
+}
