be7db28052feedcc2fd842589b8c33a748dcfd23
gambin_n <gambin_n@epitech.eu>
2014-12-16 23:17:22 +0900

    menu avancement

--- Assets/Scripts/MenuSpecificActions.js
+++ Assets/Scripts/MenuSpecificActions.js
@@ -1,12 +1,43 @@
-﻿var levelSuivant : String = "Quitter";
+﻿var action : String = "Quitter";
+var i = 0;
 
 function OnMouseUp() {
-     if (levelSuivant == "Quitter")
+     if (action == "Quitter")
      	Application.Quit();
-     else if (levelSuivant == "Resume")
+     else if (action == "ShowLevels")
+     {
+     	i = 0;
+     	while (i < 7)
+     	{
+     		GameObject.Find("LevelList").transform.GetChild(i).gameObject.SetActive(true);
+     		i++;
+     	}
+     	i = 0;
+     	while (i < 3)
+     	{
+     		GameObject.Find("MainMenu").transform.GetChild(i).gameObject.SetActive(false);
+     		i++;
+     	}
+     }
+	else if (action == "BackToMain")
+    {
+     	i = 0;
+     	while (i < 7)
+     	{
+     		GameObject.Find("LevelList").transform.GetChild(i).gameObject.SetActive(false);
+     		i++;
+     	}
+     	i = 0;
+     	while (i < 3)
+     	{
+     		GameObject.Find("MainMenu").transform.GetChild(i).gameObject.SetActive(true);
+     		i++;
+     	}
+    }	
+     else if (action == "Resume")
      	GameObject.Find("First Person Controller").SendMessage("unPause");
      else
-     	Application.LoadLevel(levelSuivant);
+     	Application.LoadLevel(action);
 }
 
 @script RequireComponent (AudioSource)
